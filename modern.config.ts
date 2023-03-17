052e09171ebf9beabb879be8a500b5f73c2d26a8
yangxingyuan <yangxingyuan@bytedance.com>
2023-03-17 10:53:34 +0800

    feat: add translation

--- modern.config.ts
+++ modern.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 function getI18nHelper(lang: "zh" | "en") {
   const cn = lang === "zh";
   // 默认语言为中文，如果是英文，需要加上 /en 前缀
+  // The default language is Chinese, if it is English, you need to add the /en prefix
   const prefix = cn ? "" : "/en";
   const getLink = (str: string) => `${prefix}${str}`;
   const getText = (cnText: string, enText: string) => (cn ? cnText : enText);
@@ -36,6 +37,7 @@ function getNavConfig(lang: "zh" | "en") {
 function getSidebarConfig(lang: "zh" | "en") {
   const { getText, getLink } = getI18nHelper(lang);
   // 注: 侧边栏配置可以嵌套，子菜单字段为 items
+  // Note: The sidebar configuration can be nested, and the sub-menu field is items
   return {
     "/": [
       {
@@ -68,13 +70,16 @@ export default defineConfig({
     logo: "https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/logo-2x-text-0104.png",
     icon: "https://lf3-static.bytednsdoc.com/obj/eden-cn/zq-uylkvT/ljhwZthlaukjlkulzlp/logo-1x-0104.png",
     // 默认语言
+    // Default language
     lang: "zh",
     themeConfig: {
       footer: {
         // 页脚的文案
+        // Footer text
         message: "© 2023 Bytedance Inc. All Rights Reserved.",
       },
       // 不同语言的配置
+      // Configuration for different languages
       locales: [
         {
           lang: "zh",
@@ -83,6 +88,7 @@ export default defineConfig({
           nav: getNavConfig("zh"),
           sidebar: getSidebarConfig("zh"),
           // 语言切换按钮的文案
+          // Language switch button text
           label: "简体中文",
         },
         {
@@ -98,4 +104,3 @@ export default defineConfig({
   },
   plugins: ["@modern-js/doc-tools"],
 });
-// 0.0.0-next-20230109041343
